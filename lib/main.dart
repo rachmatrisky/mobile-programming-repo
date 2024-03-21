b83f6c5d6912adfd11ee759eeef89d216848f222
rachmatrisky <rachmatrisky5@gmail.com>
2024-03-22 00:08:54 +0800

    add a little bit picture

--- lib/main.dart
+++ lib/main.dart
@@ -205,7 +205,20 @@ class ProductList extends StatelessWidget {
       child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
         child: Row(
-          children: [ProductCard()],
+          children: [
+            ProductCard(
+              url:
+                  'https://images.unsplash.com/photo-1600185365926-3a2ce3cdb9eb?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHNob2VzfGVufDB8fDB8fHww',
+              name: 'Nike Super',
+              price: 600000,
+            ),
+            ProductCard(
+              url:
+                  'https://images.unsplash.com/photo-1595341888016-a392ef81b7de?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHNob2VzfGVufDB8fDB8fHww',
+              name: 'Adidas Super',
+              price: 200000,
+            )
+          ],
         ),
       ),
     );
